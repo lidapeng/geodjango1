54cfeb5b03003d2b14146f58899c62923de184c5
Dapeng Li <edward.1985@163.com>
2016-03-28 23:47:29 -0600

    initial commit

---
+++ geodjango1/temp.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 26, 2016
+
+@author: edward
+'''
+#         $('#btndisplay').click(function() {    
+#             $.ajax({
+#                 url: your_url,
+#                 method: 'POST', // or another (GET), whatever you need
+#                 data: {
+#                     name: value, // data you need to pass to your function
+#                     click: True
+#                 }
+#                 success: function (data) {        
+#                     // success callback
+#                     // you can process data returned by function from views.py
+#                 }
+#             });
+#         });
